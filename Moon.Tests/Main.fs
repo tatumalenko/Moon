3b600c4ffd0bd92f98859d9ae1aa9700342cac99
tatum <tatum.alenko@gmail.com>
2020-01-26 16:51:18 -0500

    Add more main tests

--- Moon.Tests/Main.fs
+++ Moon.Tests/Main.fs
@@ -5,9 +5,35 @@ open FsUnit.Xunit
 
 [<Fact>]
 let ``Given valid command line arguments, the main function returns 0``() =
-    let result = Moon.Main.main [| "lex"; "--path"; Utils.makePath "resources/lexer/in/positivegrading.src"; "--outdir"; Utils.makePath "resources/lexer/out" |]
+    let result =
+        Moon.Main.main
+            [| "lex"
+               "--path"
+               Utils.makePath "resources/lexer/in/positivegrading.src"
+               "--outdir"
+               Utils.makePath "resources/lexer/out" |]
     result |> should equal 0
-    
+
 [<Fact>]
-let ``Given invalud command line arguments, the main function should raise error``() =
-    (fun () -> Moon.Main.main [| "lex"; "--poop" |] |> ignore) |> should throw typeof<Argu.ArguParseException> 
+let ``Given invalid cli arguments for lex subcommand, the main function should raise error``() =
+    (fun () -> Moon.Main.main [| "lex"; "--poop" |] |> ignore) |> should throw typeof<Argu.ArguParseException>
+
+[<Fact>]
+let ``Given missing path cli argument for lex subcommand, the main function should raise error``() =
+    (fun () ->
+    Moon.Main.main
+        [| "lex"
+           "--outdir"
+           Utils.makePath "resources/lexer/out" |]
+    |> ignore)
+    |> should (throwWithMessage "Missing command line argument --path") typeof<System.Exception> 
+
+[<Fact>]
+let ``Given missing outdir cli argument for lex subcommand, the main function should raise error``() =
+    (fun () ->
+    Moon.Main.main
+        [| "lex"
+           "--path"
+           Utils.makePath "resources/lexer/in/positivegrading.src" |]
+    |> ignore)
+    |> should (throwWithMessage "Missing command line argument --outDir") typeof<System.Exception> 
